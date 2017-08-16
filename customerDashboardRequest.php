1fc6d5074d50ea8d99cc477405741c2b4fbd762a
PTCL\Kazim.Jamal <Kazim.Jamal@ptcl.net.pk>
2017-08-16 16:18:01 +0500

    Fixes

--- customerDashboardRequest.php
+++ customerDashboardRequest.php
@@ -156,7 +156,7 @@
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Customer Name <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['user']?> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
